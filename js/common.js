59d3a946ca7e5383ef2f83091120514e83fb5ad6
siwon9 <siwon10823@gmail.com>
2024-03-28 13:52:18 +0900

    초기 사이트 제작중

---
+++ js/common.js
@@ -0,0 +1,15 @@
+window.addEventListener("DOMContentLoaded", function() {
+    const swiper = new Swiper(".mySwiper", {
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        
+    });
+});
+
+
+
